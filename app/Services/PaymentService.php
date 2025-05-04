055c8c676512e29b0824d9f8b3eb8eb27ce36a04
myathtut-zafir <aa.htut@gmail.com>
2025-05-04 19:00:36 +0700

    feat: add pure facade

---
+++ app/Services/PaymentService.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Services;
+
+class PaymentService
+{
+    public function kpay()
+    {
+        return "Kpay payment service";
+    }
+    public function wave()
+    {
+        return "Kpay payment service";
+    }
+}
